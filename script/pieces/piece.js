38efec1d0c5bb95a3fc52414ed9030ad3fe8cfa4
AmazingAndrewM1 <michaea3@tcnj.edu>
2024-11-18 15:01:24 -0500

    Removing unnecessary code from piece.js

--- script/pieces/piece.js
+++ script/pieces/piece.js
@@ -12,15 +12,6 @@ class Piece{
         WHITE: Symbol("WHITE"),
         BLACK: Symbol("BLACK")
     });
-    static spriteWidth;
-    static spriteHeight;
-
-    static {
-        let spriteSheet = new Image();
-        spriteSheet.src = "/images/ChessMenSpriteSheet.png";
-        Piece.spriteWidth = spriteSheet.width / Object.keys(Piece.Type).length;
-        Piece.spriteHeight = spriteSheet.height / Object.keys(Piece.Color).length;
-    }
 
     constructor(){
         throw new Error("Cannot instantiate Piece");
