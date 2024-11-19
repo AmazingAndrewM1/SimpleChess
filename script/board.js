aba7ac01286364dd4246c84b4d43c61970adbf10
AmazingAndrewM1 <michaea3@tcnj.edu>
2024-11-19 10:20:29 -0500

    The code for dragging the pieces is in place.

--- script/board.js
+++ script/board.js
@@ -37,7 +37,7 @@ class Board{
         this.pieces[7][6] = new Knight(Piece.Color.WHITE);
         this.pieces[7][7] = new Rook(Piece.Color.WHITE);
 
-        console.log(this.pieces);
+        this.isMouseDown = false;
     }
 
     static getNumRows(){
@@ -51,7 +51,6 @@ class Board{
     static getNumSquares(){
         return this.#NUM_SQUARES;
     }
-
     initializeHTML(){
         const frontEndBoard = document.getElementById("board");
 
@@ -98,6 +97,79 @@ class Board{
                     pieceDiv.ariaLabel = `${Piece.Color.getString(piece.getColor())} ${Piece.Type.getString(piece.getType())}`;
                     squares[r * Board.getNumRows() + c].appendChild(pieceDiv);
                 }
+            }
+        }
+
+        /*  Explanation of how to mimic a MouseDrag event when MouseDrag Event does not trigger because JavaScript is dumb:
+            https://techozu.com/detect-mouse-drag-javascript/#:~:text=The%20idea%20is%20very%20straightforward%3A%201%20Create%20a,was%20dragged%3B%20if%20false%2C%20it%20was%20just%20clicked.
+        */
+        frontEndBoard.addEventListener("mousedown", (event) => this.handleMouseDown(event));
+        frontEndBoard.addEventListener("mousemove", (event) => this.handleMouseMove(event));
+        frontEndBoard.addEventListener("mouseup", (event) => this.handleMouseUp(event));
+    }
+
+    /*  Even though frontEndBoard has the EventListener, event.offsetX and event.offsetY are relative to the "target" node.
+        The "target" node seems to be the innermost child element of the element that has the EventListener.
+        Use event.currentTarget to retrieve the element that has the EventListener. This is why I detest JavaScript.
+        Link to Explanation:
+        https://stackoverflow.com/questions/35360704/wrong-offsetx-and-offsety-on-mousedown-event-of-parent-element#answer-35364901
+
+        Documentation for getBoundingClientRect():
+        https://developer.mozilla.org/en-US/docs/Web/API/DOMRect#instance_properties
+    */
+    handleMouseDown(event){
+        event.preventDefault();
+
+        let rect = event.currentTarget.getBoundingClientRect();
+        let offsetX = event.clientX - rect.left;
+        let offsetY = event.clientY - rect.top;
+        let row = Math.floor(offsetY * Board.getNumRows() / rect.height);
+        let column = Math.floor(offsetX * Board.getNumColumns() / rect.width);
+
+        this.selected = {
+            row: row,
+            column: column
+        };
+
+        let square = event.currentTarget.childNodes[this.selected.row * Board.getNumRows() + this.selected.column];
+        for (const child of square.childNodes){
+            if (child.classList.contains("sprite")){
+                child.classList.add("selected");
+                child.style.transform = "translate(0px, 0px)";
+            }
+        }
+
+        this.isMouseDown = true;
+    }
+
+    handleMouseMove(event){
+        event.preventDefault();
+        if (this.isMouseDown === false){
+            return;
+        }
+
+        let square = event.currentTarget.childNodes[this.selected.row * Board.getNumRows() + this.selected.column];
+        let squareRect = square.getBoundingClientRect();
+        let offsetX = event.clientX - squareRect.left;
+        let offsetY = event.clientY - squareRect.top;
+
+        for (const child of square.childNodes){
+            if (child.classList.contains("selected")){
+                let childRect = child.getBoundingClientRect();
+                child.style.transform = `translate(${offsetX - 0.5 * childRect.width}px, ${offsetY - 0.5 * childRect.height}px)`;
+            }
+        }
+    }
+
+    handleMouseUp(event){
+        event.preventDefault();
+        this.isMouseDown = false;
+
+        let square = event.currentTarget.childNodes[this.selected.row * Board.getNumRows() + this.selected.column];
+        for (const child of square.childNodes){
+            if (child.classList.contains("sprite")){
+                child.classList.remove("selected");
+                child.removeAttribute("style");
             }
         }
     }
