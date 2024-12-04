122d71b40da976684cd5e825d82934008a4cc3c1
AmazingAndrewM1 <michaea3@tcnj.edu>
2024-12-04 12:52:46 -0500

    The bishop pseudolegal move generator works and is tested to work as intended.

--- docs/assets/script/pieces/bishop.js
+++ docs/assets/script/pieces/bishop.js
@@ -1,12 +1,34 @@
-import Piece from "./piece.js";
+import Piece, {BACK_END} from "./piece.js";
 
 class Bishop extends Piece{
     constructor(color){
         super(Piece.Type.BISHOP, color);
     }
 
-    getCaptureDirections(){
-        return [];
+    static getCaptureDirections(){
+        return [
+            {dx: 1, dy: 1},
+            {dx: 1, dy: -1},
+            {dx: -1, dy: -1},
+            {dx: -1, dy: 1}
+        ];
+    }
+
+    getPseudoLegalMoves(from){
+        let moves = [];
+
+        for (const DIRECTION of Bishop.getCaptureDirections()){
+            let square = BACK_END.getTransposed(from, DIRECTION);
+            while (square !== null && square.getPiece() === null){
+                moves.push(square);
+                square = BACK_END.getTransposed(square, DIRECTION);
+            }
+            if (square !== null && square.getPiece().getColor() !== this.color){
+                moves.push(square);
+            }
+        }
+
+        return moves;
     }
 }
 
