0cd77fa1fe8a47228c70d6d18bbbace1d7e422cf
AmazingAndrewM1 <michaea3@tcnj.edu>
2024-12-04 12:56:38 -0500

    Rook pseudolegal move generator completed. Tested to work. Castling not implemented for rook as castling is considered to be a king's move.

--- docs/assets/script/pieces/rook.js
+++ docs/assets/script/pieces/rook.js
@@ -1,12 +1,34 @@
-import Piece from "./piece.js";
+import Piece, {BACK_END} from "./piece.js";
 
 export class Rook extends Piece{
     constructor(color){
         super(Piece.Type.ROOK, color);
     }
 
-    getCaptureDirections(){
-        return [];
+    static getCaptureDirections(){
+        return [
+            {dx: 0, dy: 1},
+            {dx: 1, dy: 0},
+            {dx: 0, dy: -1},
+            {dx: -1, dy: 0}
+        ];
+    }
+
+    getPseudoLegalMoves(from){
+        let moves = [];
+
+        for (const DIRECTION of Rook.getCaptureDirections()){
+            let square = BACK_END.getTransposed(from, DIRECTION);
+            while (square !== null && square.getPiece() === null){
+                moves.push(square);
+                square = BACK_END.getTransposed(square, DIRECTION);
+            }
+            if (square !== null && square.getPiece().getColor() !== this.color){
+                moves.push(square);
+            }
+        }
+
+        return moves;
     }
 }
 
